396dc35a09b237e266f21222b0c04566e0e5b03f
Matheus Schneider <matheus.ssm@gmail.com>
2020-07-19 12:38:54 -0300

    added balance panel components structure

--- index.js
+++ index.js
@@ -2,13 +2,14 @@
  * @format
  */
 if (__DEV__) {
-  import('./config/ReactotronConfig').then(() =>
+  import('./src/config/ReactotronConfig').then(() =>
     console.log('Reactotron Configured'),
   );
 }
 
-import {AppRegistry} from 'react-native';
-import App from './srcg';
-import {name as appName} from './app.json';
+import { AppRegistry } from 'react-native';
+import App from './src';
+import Main from './src/pages/Main';
+import { name as appName } from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => Main);
