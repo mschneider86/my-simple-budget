afd93a96dc01e77e4137817354862f563fdb102a
Matheus Schneider <matheus.ssm@gmail.com>
2020-08-22 20:01:25 -0300

    added categoriers

--- src/services/Realm.js
+++ src/services/Realm.js
@@ -3,11 +3,38 @@ import Realm from 'realm';
 import CategorySchema from '../schemas/CategorySchema';
 import EntrySchema from '../schemas/EntrySchema';
 
+import {getDefaultCategories} from './Categories';
+
 export const getRealm = async () => {
   const realm = await Realm.open({
     schema: [CategorySchema, EntrySchema],
-    schemaVersion: 1,
+    schemaVersion: 2,
   });
 
   return realm;
+};
+
+export const initDB = realm => {
+  const categoriesLength = realm.objects('Category').length;
+  console.log(`initDB :: categories length: ${categoriesLength}`);
+
+  if (categoriesLength === 0) {
+    const categories = getDefaultCategories();
+
+    console.log('initDB :: initializing db...');
+
+    try {
+      realm.write(() => {
+        categories.forEach(category => {
+          console.log(
+            `initDB :: creating category: ${JSON.stringify(category)}`,
+          );
+
+          realm.create('Category', category, true);
+        });
+      });
+    } catch (error) {}
+  } else {
+    console.log('initDB :: categories already exist... Skipping.');
+  }
 };
