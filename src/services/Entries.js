90b1339ac1956c15fe1642b970ed13bb87a36a35
Matheus Schneider <matheus.ssm@gmail.com>
2020-08-24 19:30:38 -0300

    added date filter to the entries query

--- src/services/Entries.js
+++ src/services/Entries.js
@@ -1,11 +1,24 @@
-import {Alert} from 'react-native';
+enimport {Alert} from 'react-native';
 
 import {getRealm} from './Realm';
 import {getUUID} from '../services/UUID';
 
-export const getEntries = async () => {
-  const realm = await getRealm();
-  const entries = realm.objects('Entry').sorted('entryAt', true);
+tries queriimport moment from '../vendors/moment';
+
+export const getEntries = async days => {
+  let realm = await getRealm();
+
+  realm = realm.objects('Entry');
+
+  if (days > 0) {
+    const date = moment()
+      .subtract(days, 'days')
+      .toDate();
+
+    realm = realm.filtered('entryAt >= $0', date);
+  }
+
+  const entries = realm.sorted('entryAt', true);
 
   console.log('getEntries :: entries ', JSON.stringify(entries));
 
