58cc573a4331cf59a44eabd466ced67611610b76
Matheus Schneider <matheus.ssm@gmail.com>
2020-08-27 19:21:28 -0300

    added: user location using google Geocoder api

--- src/Pages/NewEntry/NewEntryAddressPicker/index.js
+++ src/Pages/NewEntry/NewEntryAddressPicker/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import {View, TouchableOpacity} from 'react-native';
-
+import {View, TouchableOpacity, Alert} from 'react-native';
+import Geocoder from 'react-native-geocoding';
 import Geolocation from '@react-native-community/geolocation';
 import {Icon} from 'react-native-vector-icons/MaterialIcons';
 
@@ -9,18 +9,45 @@ import Colors from '../../../styles/Colors';
 import styles from './styles';
 
 export default function NewEntryAddressPicker() {
-  const onButtonPress = () => {
+  const getLocation = (latitude, longitude) => {
+    Geocoder.init('INSERT_GOOGLE_API_HERE');
+
+    Geocoder.from({latitude, longitude})
+      .then(json => {
+        const formattedAddress = json.results[0].formattedAddress;
+      })
+      .catch(error => {
+        console.error(
+          'NewEntryAddressPicker :: getLocation :: error getting user location',
+          error,
+        );
+        Alert.alert(
+          'Error recovering your position, please check that you have given the required permission to the app and try again!',
+        );
+      });
+  };
+
+  const getPosition = () => {
     Geolocation.getCurrentPosition(
       pos => {
         const [latitude, longitude] = pos.coords;
+
+        getLocation(latitude, longitude);
       },
       error => {
         console.error(
-          'NewEntryAddressPicker :: error getting user location',
+          'NewEntryAddressPicker :: getPosition :: error getting user location',
           error,
+        );
+        Alert.alert(
+          'Error recovering your position, please check that you have given the required permission to the app and try again!',
         );
       },
     );
+  };
+
+  const onButtonPress = () => {
+    getPosition();
   };
   return (
     <View>
