9c9b351402c1c29f9a234a0c6e4e5ffedc767a98
Matheus Schneider <matheus.ssm@gmail.com>
2020-08-30 17:17:45 -0300

    updated: change react navigation version

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,8 @@
-import {createAppContainer, createSwitchNavigator} from 'react-navigation';
+import React from 'react';
+import {createStackNavigator} from '@react-navigation/stack';
+import {NavigationContainer} from '@react-navigation/native';
+
+const Stack = createStackNavigator();
 
 import Main from './pages/Main';
 import NewEntry from './pages/NewEntry';
@@ -6,20 +10,26 @@ import Report from './pages/Report';
 import Welcome from './pages/Welcome';
 import Loading from './pages/Loading';
 
-const Routes = createAppContainer(
-  createSwitchNavigator(
-    {
-      Loading,
-      Welcome,
-      Main,
-      NewEntry,
-      Report,
-    },
-    {
-      initialRouteName: 'Loading',
-      backBehavior: 'history',
-    },
-  ),
-);
+const StackScreens = () => {
+  return (
+    <Stack.Navigator
+      initialRouteName="Loading"
+      screenOptions={{headerShown: false}}>
+      <Stack.Screen name="Loading" component={Loading} />
+      <Stack.Screen name="Welcome" component={Welcome} />
+      <Stack.Screen name="Main" component={Main} />
+      <Stack.Screen name="NewEntry" component={NewEntry} />
+      <Stack.Screen name="Report" component={Report} />
+    </Stack.Navigator>
+  );
+};
+
+const Routes = () => {
+  return (
+    <NavigationContainer>
+      <StackScreens />
+    </NavigationContainer>
+  );
+};
 
 export default Routes;
